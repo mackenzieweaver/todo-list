50192b6f5e831b7ccc8e0dfedcb71bf8f5e927ce
mackenzieweaver <macknz7@gmail.com>
2019-05-23 15:04:01 -0400

    Initial commit

---
+++ precache-manifest.9255d3946c8631e0f9575b9582d360f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea13200f057c39e509e12c13e9b56d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "378217c16964a7a24251",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "3522c9939ecc4fbbc96a",
+    "url": "/static/js/2.2939912b.chunk.js"
+  },
+  {
+    "revision": "378217c16964a7a24251",
+    "url": "/static/js/main.f3329850.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
